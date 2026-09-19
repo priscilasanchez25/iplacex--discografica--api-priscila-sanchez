2506a84eb056121694eca2587f728daf168c21fc
priscilasanchez25 <priscila.sanchez@alumno.iplacex.cl>
2026-09-19 01:42:23 -0300

    Update DiscografiaApplication.java

--- src/main/java/org/iplacex/proyectos/discografia/DiscografiaApplication.java
+++ src/main/java/org/iplacex/proyectos/discografia/DiscografiaApplication.java
@@ -1,29 +1,13 @@
 package org.iplacex.proyectos.discografia;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DiscografiaApplication implements CommandLineRunner {
+public class DiscografiaApplication {
 
-    @Value("${spring.data.mongodb.uri}")
-    private String mongoUri;
+	public static void main(String[] args) {
+		SpringApplication.run(DiscografiaApplication.class, args);
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(DiscografiaApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) {
-        String safeUri = mongoUri.replaceAll(
-                "://([^:]+):([^@]+)@",
-                "://$1:****@"
-        );
-
-        System.out.println("========================================");
-        System.out.println("MONGO URI: " + safeUri);
-        System.out.println("========================================");
-    }
 }
